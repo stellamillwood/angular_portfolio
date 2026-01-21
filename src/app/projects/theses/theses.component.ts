a7f9e8bb895b69164468c489dcf7dd2abda0b517
Stella <stella.millwood@gmail.com>
2026-01-21 16:33:21 +0100

    fixed styling for buttons on thesis page

--- src/app/projects/theses/theses.component.ts
+++ src/app/projects/theses/theses.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { PROJECTS, Project } from '../projects-data';
 
+// Angular Material imports
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-theses',
   standalone: true,
   templateUrl: './theses.component.html',
+  imports: [MatIconModule, MatButtonModule],
   styleUrl: './theses.component.css',
 })
 export class ThesesComponent {
